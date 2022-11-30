5c6a39abc2459c82af17e0820c4096746364c7d5
Pablo <pablomelo.inf@gmail.com>
2022-11-30 02:04:45 -0300

    created migrates, models and basic structure project

---
+++ src/config/database.js
@@ -0,0 +1,16 @@
+module.exports = {
+  host: '172.17.0.3',
+  username: 'docker',
+  password: 'docker',
+  database: 'nodeauth',
+  dialect: 'postgres',
+  //operatorsAliases: false,
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+
+  }
+
+};
