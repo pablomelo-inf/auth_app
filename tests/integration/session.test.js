bd2d19b2b4a5c65e05e2444a83f3c6d23cc9cd9d
Pablo <pablomelo.inf@gmail.com>
2022-12-01 01:10:22 -0300

    first test using TDD: red step

--- tests/integration/session.test.js
+++ tests/integration/session.test.js
@@ -1,37 +1,28 @@
+const request = require('supertest');
 
+const app = require('../../src/app');
 const { User } = require('../../src/app/models');
 
-
 describe('Authentication', () => { 
-    /*it('should receive jwt token when authenticated with valid credentials', () => {
-    
-    });
-
-    it('should sum two numbers', () => {
-        const y = 2;
-        const x = 4;
-
-        const sum = x + y;
-
-        expect(sum).toBe(76);
-    });*/
+    it('should authenticate with valid credentials', async () =>{
+        const user = await User.create({
+            name: 'PSM',
+            email: 'psm@gmail.com',
+            password_hash: '12451'
+        });
 
 
+        const response = await request(app)
+                         .post("/sessions")
+                         .send({
+                            email: user.email,
+                            password: '12451'
+                         });
+        
+        expect(response.status).toBe(200);
 
-    it('should add data db', async () => {
-       const user = await User.create({
-            name: "PSM2",
-            email: "psm@gmail.com",
-            password_hash: "123432423"
-       });
-
-       console.log(user);
-
-       expect(user.email).toBe("psm@gmail.com");
 
     });
-
-
- })
+})
 
 
