b860c4ceaf5ed0d1c8a3d478ea42af439705430e
Pablo <pablomelo.inf@gmail.com>
2022-12-01 01:27:27 -0300

    first test using TDD: green step

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,9 @@
 const routes = require('express').Router();
 const {User} = require('./app/models');
 
-User.create({
-    name: "PSM",
-    email: "psm@gmail.com",
-    password_hash: "129032423345rsHTVBdf"
-})
+
+const SessionController = require('./app/controllers/SessionController');
+
+routes.post('/sessions', SessionController.store);
 
 module.exports = routes;
--- tests/integration/session.test.js
+++ tests/integration/session.test.js
@@ -2,8 +2,15 @@ const request = require('supertest');
 
 const app = require('../../src/app');
 const { User } = require('../../src/app/models');
+const truncate = require('../utils/truncate');
+
 
 describe('Authentication', () => { 
+
+    beforeEach(async () => {
+        await truncate();
+    });
+
     it('should authenticate with valid credentials', async () =>{
         const user = await User.create({
             name: 'PSM',
