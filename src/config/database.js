7659b4a94a4b56c262f446463ffccf986f8009f3
Pablo <pablomelo.inf@gmail.com>
2022-12-01 01:01:50 -0300

    first test integration using database sqlite

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,15 @@
+require('dotenv').config({
+  path: process.env.NODE_ENV = 'test' ? '.env.test' : '.env'
+});
+  
+
 module.exports = {
-  host: '172.17.0.3',
-  username: 'docker',
-  password: 'docker',
-  database: 'nodeauth',
-  dialect: 'postgres',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  dialect: process.env.DB_DIALECT || "postgres",
+  storage: './tests/database.sqlite',
   //operatorsAliases: false,
   logging: false,
   define: {
